c96bed9d602178e758c8cfa04c1f616f8c2c3f04
ebrahim zeinali <106522566+ebi1317@users.noreply.github.com>
2023-01-08 16:11:19 +0100

    Add files via upload

---
+++ Sum_Of_dig.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int SumDigits(int inputNum, int& sum)
+{
+   if( inputNum != 0)
+   {
+       sum = sum + inputNum%10;
+       inputNum = inputNum /10;
+       SumDigits(inputNum,sum);
+
+   }
+   else
+   {
+       return inputNum;
+   }
+}
+
+
+
+
+
+int main()
+{
+    int number = 0;
+    int Sum = 0;
+    cout << "please input number:   " << endl;
+    cin >> number;
+
+
+    int n =  SumDigits(number, Sum);
+
+    cout <<"sum of digits is: " << Sum << endl;
+
+
+
+
+
+
+}
